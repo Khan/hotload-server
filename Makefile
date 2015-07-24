4e854944085fbd9d491855bcb11ebb1e88b14b03
William Chargin <wchargin@gmail.com>
2015-07-23 19:42:59 -0700

    move to a more node-y setup with package.json
    Summary: "make" is now "npm run build"
    
    Reviewers: desmond
    
    Reviewed By: desmond
    
    Differential Revision: https://phabricator.khanacademy.org/D19826

--- Makefile
+++
@@ -1,7 +0,0 @@
-dist/index.js: src
-	./build.sh
-
-.PHONY: deps
-deps:
-	npm install
-	mkdir -p dist
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Usage:
-#     ./build.sh [--watch]
-cd $(dirname "$0")
-babel src/ --out-file dist/index.js "$@"
