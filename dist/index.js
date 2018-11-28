e8f0ea1bb0b215b204ddc30be14783eca89f6265
Craig Silverstein <csilvers@khanacademy.org>
2018-11-27 16:24:02 -0800

    Update the deps to avoid security issues.
    Summary:
    I did this by running
       npm i
       npm audit fix --force
    I had to fix the babel dep by hand.
    
    Test Plan:
    I ran
       npm run build
    without error, and
       dist/index.js --port 3000 "/tmp/*.txt"
    without error as well.
    
    Reviewers: drosile
    
    Reviewed By: drosile
    
    Differential Revision: https://phabricator.khanacademy.org/D50414

--- dist/index.js
+++ dist/index.js
@@ -1,38 +1,37 @@
-'use strict';
+/* eslint-disable no-console */
 
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var debounce = require('debounce');
-var chokidar = require('chokidar'); // file watching
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const debounce = require('debounce');
+const chokidar = require('chokidar'); // file watching
 
-var argv = require('yargs').usage('Usage: $0 --port <port> <globs-to-watch> [ ... ]').example('$0 --port 3000 "dir/**/*.txt"').demand(1).demand('port').alias('port', 'p').describe('port', 'port on which to listen for WebSocket connections').help('h', 'help').argv;
+const argv = require('yargs').usage('Usage: $0 --port <port> <globs-to-watch> [ ... ]').example('$0 --port 3000 "dir/**/*.txt"').demand(1).demand('port').alias('port', 'p').describe('port', 'port on which to listen for WebSocket connections').help('h', 'help').argv;
 
-var port = argv.port;
-var globs = argv._;
+const { port, _: globs } = argv;
 
-console.log('Watching for changes to ' + globs.join(', ') + '.');
+console.log(`Watching for changes to ${globs.join(", ")}.`);
 
-io.on('connection', function (socket) {
-    var watcher = chokidar.watch(globs);
+io.on('connection', socket => {
+    const watcher = chokidar.watch(globs);
 
-    var changedFiles = {}; // set as object
-    var sendChanges = debounce(function () {
-        socket.emit('files changed', Object.keys(changedFiles));
+    let changedFiles = {}; // set as object
+    const sendChanges = debounce(() => {
+        socket.emit("files changed", Object.keys(changedFiles));
         changedFiles = {};
     }, 250);
-    var handler = function handler(name) {
-        console.log('Changed: ' + name);
+    const handler = name => {
+        console.log("Changed: " + name);
         changedFiles[name] = true;
         sendChanges();
     };
 
-    watcher.on('ready', function () {
+    watcher.on('ready', () => {
         watcher.on('add', handler);
         watcher.on('change', handler);
     });
 
-    socket.on('disconnect', function () {
+    socket.on('disconnect', () => {
         watcher.close();
     });
 });
@@ -42,6 +41,6 @@ app.get('/_api/ping', function (req, res) {
     res.send('pong');
 });
 
-http.listen(port, function () {
-    console.log('Server started on port ' + port + '.');
+http.listen(port, () => {
+    console.log(`Server started on port ${port}.`);
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
